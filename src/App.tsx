7a77e90da97facdb96e22ef8dc0e01be057e6e54
zembahk <77575523+zembahk@users.noreply.github.com>
2022-10-01 18:24:20 -0700

    css

--- src/App.tsx
+++ src/App.tsx
@@ -7,42 +7,43 @@ class Land {
   image: any
   yield: any
   resource: any 
-  cords: any
-  x: any
-  y: any
-  z: any
+  cords: Map<string, number> = new Map()
   defence: any
   settlement: any 
   bonus: any 
+  total: any
 }
 
+const opensea = 'https://opensea.io/assets/ethereum/0x17d084106c2f1c716ce39fa015ab022757d30c9a/'
+const metadataIpfs = 'QmdDumXc55DAfMCfdhaghZQgb7cpCTTRyS5vmo3dbzUEnP'
+
 const getData = async (tokenId:string) => {
-  const data = await fetch('https://dweb.link/ipfs/QmdDumXc55DAfMCfdhaghZQgb7cpCTTRyS5vmo3dbzUEnP/' + tokenId + '.json')
+  const data = await fetch('https://dweb.link/ipfs/' + metadataIpfs +'/' + tokenId + '.json' )
   .then((response) => response.json())
   .then((data) => {  return data });
   let someLand = new Land()
   someLand.tokenId = tokenId
-  someLand.image = data.image
-  if (someLand.image?.substring(0,7) === 'ipfs://') { 
-    someLand.image = 'https://dweb.link/ipfs/' + someLand.image?.slice(7, someLand.image.length)
-  }
+  someLand.image = 'https://dweb.link/ipfs/' + data.image?.slice(7, data.image.length)
   const attributes = JSON.parse(JSON.stringify(data.attributes))
   attributes.forEach((a: any) => {
-    if (a.trait_type === 'DEFENCE')      { someLand.defence = a.value }
-    if (a.trait_type === 'RESOURCE')     { someLand.resource = a.value }
-    if (a.trait_type === 'Yield')        { someLand.yield = a.value }
-    if (a.trait_type === 'Village')      { someLand.settlement = a.trait_type; someLand.bonus = a.value }
-    if (a.trait_type === 'Town')         { someLand.settlement = a.trait_type; someLand.bonus = a.value }
-    if (a.trait_type === 'City')         { someLand.settlement = a.trait_type; someLand.bonus = a.value }
-    if (a.trait_type === 'Capital')      { someLand.settlement = a.trait_type; someLand.bonus = a.value }
-    if (a.trait_type === 'COORDINATES')  { someLand.cords = a.value 
-        const cords = a.value.split(' ')
-    someLand.x = cords[0]
-    someLand.y = cords[1]
-    someLand.z = cords[2]
+    const tt = a.trait_type
+    if (tt === 'DEFENCE')  { someLand.defence = a.value }
+    if (tt === 'RESOURCE') { someLand.resource = a.value }
+    if (tt === 'Yield')    { someLand.yield = a.value }
+    if (tt === 'COORDINATES') 
+    { const cords = a.value.split(' ')
+      someLand.cords.set('x', cords[0])
+      someLand.cords.set('y', cords[1])
+      someLand.cords.set('z', cords[2])
     }
+    if (tt === 'Village'|| tt === 'Town' || tt === 'City' || tt === 'Capital') 
+      { someLand.settlement = tt
+        someLand.bonus = a.value 
+      }
   })
-
+  if(someLand.bonus !== undefined) {
+    someLand.total = someLand.bonus/100 * someLand.yield + someLand.yield
+  }
   return someLand
 }
 
@@ -52,30 +53,37 @@ function App() {
     if (!land) {return <></>}
     return (
       <div className='land'>
-        <img src={land.image} alt={land.tokenId} width='40%' />
-        <br/>Token ID : {land.tokenId}<br/>
-        <div hidden={land.defence === 'None'}>
-          Defence : {land.defence}
-        </div>
-        <div hidden={!land.resource}>
-          {land.resource} Yield : {land.yield}
-        </div>
-        <div hidden={!land.cords}>
-          X : {land.x} <br/>
-          Y : {land.y} <br/>
-          Z : {land.z}<br/>
-        </div>
+        <h6>
+          Token ID : {land.tokenId} <br/>
+          <div hidden={land.defence === 'None'}>
+            Defence : {land.defence}
+          </div>
+          <div className='cords'>
+            <div className='x'>X : {land.cords.get('x')} </div>
+            <div className='y'>Y : {land.cords.get('y')} </div>
+            <div className='z'>Z : {land.cords.get('z')} </div>
+          </div> 
+          <br/>
+          Total {land.resource} Yield : 
+          <span className='yield'>{land.bonus  ? land.total : land.yield}</span>
+        </h6>
         <div hidden={!land.settlement}>
           {land.settlement} Bonus : {land.bonus}%
         </div>
+        <picture>
+          <a href={opensea + land.tokenId} title='Opensea.io'>
+            <img src={land.image} alt={land.tokenId} width='50%' />
+          </a>
+        </picture> 
       </div>
+      
     )
   }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <input id='idInput' className='id' placeholder='token id #'  
+        <h4>Kryptoria Land Tokens</h4>
+        <input id='idInput' className='id' placeholder='id #'  
           onChange={async (e: React.ChangeEvent<HTMLInputElement>)=> {
             e.preventDefault();
             const id = e.target.value
@@ -84,8 +92,10 @@ function App() {
             } 
          }}
         ></input>
-        <h4>Kryptoria Land Tokens</h4>
-        <Display />
+        <br/>
+        <img src={logo} className="App-logo" alt="logo" />
+        <Display /> 
+        <p/>
       </header>
     </div>
   );
